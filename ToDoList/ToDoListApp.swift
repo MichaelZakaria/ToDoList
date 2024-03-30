a5d81e91bc2bcffe1ad97a026874c95871681dc3
MichaelZakaria <65913937+MichaelZakaria@users.noreply.github.com>
2024-03-30 17:44:37 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Marco Zakaria on 30/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
